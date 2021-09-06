3c990a70f1fe993012ef6a703d80931ead7100ed
Graeme Diack <graemediack@hotmail.com>
2021-09-06 20:09:09 +0000

    intersection script - nightly routine

---
+++ src/automation/intersections.R
@@ -0,0 +1,152 @@
+require(tidyverse)
+require(sf)
+require(leaflet)
+require(crosstalk)
+require(DT)
+require(shiny)
+require(shinydashboard)
+require(RPostgres)
+require(DBI)
+require(neo4r)
+
+source("./src/secrets.R",local = TRUE)
+
+# Custom functions
+source("./src/gisOperations.R",local = TRUE)
+
+#########################
+# Pull data from SQL database using function from gisOperations
+
+ICES_Ecoregions <- loadFullWKBData("ices_ecoregions")
+
+nafoDivisionsSF <- loadFullWKBData("nafo_divisions")
+
+icesStatEcoSF <- loadFullWKBData("ices_stat_rect_eco")
+
+migrationSF <- loadFullWKBData("buffered_seasalar_migration")
+
+feedingSF <- loadFullWKBData("feeding_zones")
+##########################
+
+##############################
+# IMPORT METADATA FROM GRAPH
+neo_con <- neo4j_api$new(url = paste("http://",NEO4J_HOST,":",NEO4J_PORT,sep = ""),
+                         user = NEO4J_USER,
+                         password = NEO4J_PASSWD) 
+LSFMetadata <- call_neo4j("MATCH (m:Metadata) RETURN m,id(m) as id ORDER BY id;",neo_con,type = 'row')
+LSFMetadataTibble <- LSFMetadata$m
+LSFMetadataTibble$id <- LSFMetadata$id$value
+# CONVERT LON/LATS to INTEGERS
+# TODO make these data integers within the graph
+LSFMetadataTibble$metadataCoverageSouth <- as.double(LSFMetadataTibble$metadataCoverageSouth)
+LSFMetadataTibble$metadataCoverageNorth <- as.double(LSFMetadataTibble$metadataCoverageNorth)
+LSFMetadataTibble$metadataCoverageWest <- as.double(LSFMetadataTibble$metadataCoverageWest)
+LSFMetadataTibble$metadataCoverageEast <- as.double(LSFMetadataTibble$metadataCoverageEast)
+#
+##############################
+
+#############################
+# Create polygons for each row in LSFMetadataTibble
+
+#set up a geometry column with dummy polygon to house the soon to be created real polygons
+LSFMetadataTibble$geometry <- st_sfc(st_polygon(list(rbind(c(0,0),c(1,0),c(1,-1),c(0,-1),c(0,0)))))
+LSFMetadataTibble$centre <- "POINT (0,0)" # create centre point column to accept as WKT
+# for loop to create individual polygons per tibble row
+for(i in 1:nrow(LSFMetadataTibble)){
+  # rbind list created from the 4 lon/lat columns in the data
+  # note polygon standard clockwise, 5 points for 4 sided polygon WestNorth, EastNorth, EastSouth, WestSouth, WestNorth
+  thislist <- list(rbind(c(LSFMetadataTibble$metadataCoverageWest[i],LSFMetadataTibble$metadataCoverageNorth[i]),
+             c(LSFMetadataTibble$metadataCoverageEast[i],LSFMetadataTibble$metadataCoverageNorth[i]),
+             c(LSFMetadataTibble$metadataCoverageEast[i],LSFMetadataTibble$metadataCoverageSouth[i]),
+             c(LSFMetadataTibble$metadataCoverageWest[i],LSFMetadataTibble$metadataCoverageSouth[i]),
+             c(LSFMetadataTibble$metadataCoverageWest[i],LSFMetadataTibble$metadataCoverageNorth[i])))
+  
+  # use st_polygon to convert to simple feature polygon object
+  thispolygon <- st_polygon(thislist)
+  
+  # pass in to replace dummy entry in geometry column
+  LSFMetadataTibble$geometry[i] <- thispolygon
+  LSFMetadataTibble$centre[i] <- st_as_text(st_centroid(thispolygon)) # Calculate centroids
+  # Return calculated centroids to graph
+  query <- paste0("MATCH (m:Metadata) WHERE id(m) = ",LSFMetadataTibble$id[i]," SET m.metadataCoverageCentroid = '",LSFMetadataTibble$centre[i],"';")
+  call_neo4j(query,neo_con,type = 'row')
+}
+
+# convert full tibble to sf object and set CRS
+LSFMetadataTibble <- sf::st_as_sf(LSFMetadataTibble,crs=4326)
+
+######################
+######################
+# Calculate intersects
+# st_intersects returns a Sparse geometry binary predicate list
+# This is basically a list as long as the first argument passed
+# and as wide as the second, with varying row width based on how many intersects occur
+ecoregionIntersects <- sf::st_intersects(LSFMetadataTibble,ICES_Ecoregions)
+nafodivisionIntersects <- sf::st_intersects(LSFMetadataTibble,nafoDivisionsSF)
+icesrectanglesIntersects <- sf::st_intersects(LSFMetadataTibble,icesStatEcoSF)
+migrationrouteIntersects <- sf::st_intersects(LSFMetadataTibble,migrationSF)
+
+
+# Visualize some Overlaps and Intersections
+
+leaflet () %>%
+  addProviderTiles(leaflet::providers$Esri.OceanBasemap) %>%
+  addPolygons(data = LSFMetadataTibble[1,],
+              color = "green", weight = 1)  %>%
+  addPolygons(data = ICES_Ecoregions[ecoregionIntersects[[1]],],
+              color = "red", weight = 1)
+
+leaflet () %>%
+  addProviderTiles(leaflet::providers$Esri.OceanBasemap) %>%
+  addPolygons(data = LSFMetadataTibble[34,],
+              color = "green", weight = 1)  %>%
+  addPolygons(data = nafoDivisionsSF[nafodivisionIntersects[[34]],],
+              color = "red", weight = 1)
+
+
+###############################
+# Pass these intersects back to the graph
+#ECOREGIONS
+for(i in 1:nrow(ecoregionIntersects)){
+  query <- paste0("MATCH (m:Metadata) WHERE id(m) = ",
+                  LSFMetadataTibble$id[i],
+                  " SET m.metadataCoverageIntersectICESEcoRegion = '",
+                  paste0(ICES_Ecoregions$ecoregion[ecoregionIntersects[[i]]],collapse = ','),
+                  "';")
+  #print(query)
+  call_neo4j(query,neo_con,type = 'row')
+}
+#NAFODIVISIONS
+for(i in 1:nrow(nafodivisionIntersects)){
+  query <- paste0("MATCH (m:Metadata) WHERE id(m) = ",
+                  LSFMetadataTibble$id[i],
+                  " SET m.metadataCoverageIntersectNAFODivision = '",
+                  paste0(nafoDivisionsSF$zone[nafodivisionIntersects[[i]]],collapse = ','),
+                  "';")
+  #print(query)
+  call_neo4j(query,neo_con,type = 'row')
+}
+
+#MIGRATIONROUTES_BUFFERED - ogc_fid only
+for(i in 1:nrow(migrationrouteIntersects)){
+  query <- paste0("MATCH (m:Metadata) WHERE id(m) = ",
+                  LSFMetadataTibble$id[i],
+                  " SET m.metadataCoverageIntersectMigrationRoutes = '",
+                  paste0(migrationSF$ogc_fid[migrationrouteIntersects[[i]]],collapse = ','),
+                  "';")
+  #print(query)
+  call_neo4j(query,neo_con,type = 'row')
+}
+#MIGRATIONROUTES_BUFFERED - combined current/month
+# create bodged column with current and month combined
+migrationSF$currentMonth <- paste(migrationSF$current," (",migrationSF$month,")",sep = "")
+for(i in 1:nrow(migrationrouteIntersects)){
+  query <- paste0("MATCH (m:Metadata) WHERE id(m) = ",
+                  LSFMetadataTibble$id[i],
+                  " SET m.metadataCoverageIntersectMigrationRoutes = '",
+                  paste0(unique(migrationSF$currentMonth[migrationrouteIntersects[[i]]]),collapse = ','),
+                  "';")
+  #print(query)
+  call_neo4j(query,neo_con,type = 'row')
+}
+
