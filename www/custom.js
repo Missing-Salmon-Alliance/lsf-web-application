f81d85c9f8d7d28caa9b07e6dd0a45e9f9c19e16
Graeme Diack <graemediack@hotmail.com>
2021-12-18 00:02:36 +0000

    improve table click remove tooltip

--- www/custom.js
+++ www/custom.js
@@ -2,6 +2,11 @@
 // specificly for the geographicExplore map and linked to onRender defined as 'map'
 // https://stackoverflow.com/questions/70288989/programatically-trigger-marker-mouse-click-event-in-r-leaflet-for-shiny/
 shinyjs.markerClick = function(id) {
+                        // remove any tooltips that are present to prevent them cluttering the map
+                        map.eachLayer(function(layer) {
+                          if(layer.options.pane === "tooltipPane") layer.removeFrom(map);
+                        });
+                        // activate a click on the relevant map marker
                         map.eachLayer(function (layer) {
                           if (layer.options.layerId == id) {
                             layer.fire("click");
